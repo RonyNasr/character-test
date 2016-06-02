139d471c126d7b862cc8c89503c9cf4620a4c61b
Rony Nasr <nasr.rony@gmail.com>
2016-06-02 14:57:44 -0700

    added initial files and folders

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business (or back-end) logic:
+
+
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    //var number1 = parseInt($("#input1").val());
+
+  });
+});
